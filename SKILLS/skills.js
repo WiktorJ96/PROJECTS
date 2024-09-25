1c0fe8887cab97e8e65118188d5fff791d7e7a38
Wiktor.J <testyrecenzje@gmail.com>
2024-09-25 18:33:49 +0200

    update

--- SKILLS/skills.js
+++ SKILLS/skills.js
@@ -1,22 +1,22 @@
 const $ = (selector) => document.querySelector(selector);
-const scrollbar = $('#custom-scrollbar');
-    let isScrolling;
+const scrollbar = $("#custom-scrollbar");
+let isScrolling;
 
-    window.addEventListener('scroll', function () {
-        scrollbar.style.opacity = '1';
+window.addEventListener("scroll", function () {
+  scrollbar.style.opacity = "1";
 
-        const windowHeight = window.innerHeight;
-        const documentHeight = document.documentElement.scrollHeight;
-        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.documentElement.scrollHeight;
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-        const scrollPercentage = (scrollTop / (documentHeight - windowHeight)) * 100;
-        const scrollbarHeight = (windowHeight / documentHeight) * 100;
+  const scrollPercentage = (scrollTop / (documentHeight - windowHeight)) * 100;
+  const scrollbarHeight = (windowHeight / documentHeight) * 100;
 
-        scrollbar.style.height = scrollbarHeight + '%';
-        scrollbar.style.top = scrollPercentage + '%';
+  scrollbar.style.height = scrollbarHeight + "%";
+  scrollbar.style.top = scrollPercentage + "%";
 
-        clearTimeout(isScrolling);
-        isScrolling = setTimeout(() => {
-            scrollbar.style.opacity = '0';
-        }, 1000);
-    });
+  clearTimeout(isScrolling);
+  isScrolling = setTimeout(() => {
+    scrollbar.style.opacity = "0";
+  }, 1000);
+});
