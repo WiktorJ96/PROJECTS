f582acae2e15177d0bf5493e14a96f5a62d32d9b
Wiktor.J <testyrecenzje@gmail.com>
2026-05-31 01:30:33 +0200

    up

--- docs/Vault/dev/start-vault.bat
+++ docs/Vault/dev/start-vault.bat
@@ -1,40 +1,28 @@
 @echo off
+setlocal
 
-REM Sprawdzenie, czy istnieje nowa wersja obrazu z repozytorium (opcjonalne)
-docker-compose pull
+where docker >nul 2>nul
+if errorlevel 1 (
+  echo Docker is not available in PATH.
+  pause
+  exit /b 1
+)
 
-REM Zatrzymanie kontenerów, jeśli są uruchomione
-docker-compose down
+docker compose version >nul 2>nul
+if errorlevel 1 (
+  echo Docker Compose v2 is not available.
+  pause
+  exit /b 1
+)
 
-REM Uruchomienie istniejącego obrazu bez jego przebudowywania, jeśli nie ma zmian
-docker-compose up -d --no-recreate
+set DOCKER_BUILDKIT=1
+set COMPOSE_DOCKER_CLI_BUILD=1
 
-REM Otwarcie aplikacji w domyślnej przeglądarce
+docker compose pull mongo
+docker compose build app
+docker compose up -d --wait
+if errorlevel 1 docker compose up -d
+
 start "" "http://localhost:3000"
 
 pause
-
-@REM @echo off
-@REM setlocal
-
-@REM REM --- Szybsze buildy (BuildKit) ---
-@REM set DOCKER_BUILDKIT=1
-@REM set COMPOSE_DOCKER_CLI_BUILD=1
-
-@REM REM 1) Pobierz tylko obrazy z rejestru (dotyczy m.in. mongo)
-@REM docker compose pull mongo
-
-@REM REM 2) Zbuduj obraz aplikacji (korzysta z cache warstw)
-@REM docker compose build --pull app
-
-@REM REM 3) Uruchom stack; poczekaj na HEALTHCHECK (Compose v2)
-@REM docker compose up -d --wait || docker compose up -d
-
-@REM REM 4) Otwórz aplikację w przeglądarce
-@REM start "" "http://localhost:3000"
-
-@REM REM 5) Zainicjuj uruchomienie MongoDB Compass przez URI
-@REM start "" "mongodb://localhost:27017/dbvault"
-
-@REM pause
-
